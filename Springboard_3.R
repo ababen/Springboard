ce39b13b967c794916219c6b01f074e83ed89a6b
ababen <alexbaben@gmail.com>
2016-08-23 01:48:26 -0400

    Added example for sub

--- Springboard_3.R
+++ Springboard_3.R
@@ -9,4 +9,14 @@ View(refine_original)
 refine1 <- refine_original
 refine2 <- unite(refine1,full_address,c("address","city"), sep = ",",remove = TRUE)
 refine2 <- unite(refine1,full_address,c("address":"country"), sep = ",",remove = TRUE)
-refine3 <- gsub("tut","ot",company,ignore.case=T))
+
+# Please run these two steps together to see the problem.
+
+refine3 <- gsub("ak zo","akzo",refine1,ignore.case=T)
+refine3 # I don not understand why gsub placed the 
+
+# This is the example I am working off of:
+
+#Case insensitive replace:
+#  > gsub("tut","ot",x,ignore.case=T))
+#[1] "R otorial"
