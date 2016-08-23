cefaff5de69da2b34ea40a624ed0aef40f24daca
ababen <alexbaben@gmail.com>
2016-08-23 00:54:45 -0400

    Add files via upload

--- Springboard_3.R
+++ Springboard_3.R
@@ -1,8 +1,12 @@
-refine_original <- read.csv("C:/Users/ababen/Downloads/refine_original.csv")
-View(refine_original)
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("devtools")
 library("tidyr", lib.loc="~/R/win-library/3.3")
 library("dplyr", lib.loc="~/R/win-library/3.3")
 library(devtools)
+refine_original <- read.csv("C:/Users/ababen/Downloads/refine_original.csv")
+View(refine_original)
+refine2 <- refine_original
 refine2 <- unite(refine1,full_address,c("address","city"), sep = ",",remove = TRUE)
 refine2 <- unite(refine1,full_address,c("address":"country"), sep = ",",remove = TRUE)
 
