a1a48fd41e3f324ddda4ed550484769d7fa54de8
Alexander Babenchuk <alexbaben@gmail.com>
2016-09-13 20:28:58 -0400

    Setup Ex. #3

---
+++ Ex 3/run_analysis.R
@@ -0,0 +1,7 @@
+#1. Merges the training and the test sets to create one data set.
+
+#2. Extracts columns containing mean and standard deviation for each measurement (Hint: Since some feature/column names are repeated, you may need to use the make.names() function in R)
+
+#3. Creates variables called ActivityLabel and ActivityName that label all observations with the corresponding activity labels and names respectively
+
+#4. From the data set in step 3, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
