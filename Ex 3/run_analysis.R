53120f85238bddd9291e8c531eb2fe7445581d32
Alexander Babenchuk <alexbaben@gmail.com>
2016-09-20 18:49:58 -0400

    IF Statement issues

--- Ex 3/run_analysis.R
+++ Ex 3/run_analysis.R
@@ -6,8 +6,8 @@ library(stringr)
 setwd("C:/Users/ababen/OneDrive/Documents/R/Springboard/Springboard/Ex 3")
 
 # Load datasets
-X_test <- read.table("dataset/test/X_test.txt", quote="\"", comment.char="")
-X_train <- read.table("dataset/train/X_train.txt", quote="\"", comment.char="")
+x_test <- read.table("dataset/test/X_test.txt", quote="\"", comment.char="")
+x_train <- read.table("dataset/train/X_train.txt", quote="\"", comment.char="")
 
 y_test <- read.table("dataset/test/y_test.txt", quote="\"", comment.char="")
 y_train <- read.table("dataset/train/y_train.txt", quote="\"", comment.char="")
@@ -19,11 +19,18 @@ features <- read.table("dataset/features.txt", quote="\"", comment.char="")
 
 #1. Merges the training and the test sets to create one data set.
 
-ds_X <- rbind(X_test,X_train)
+x_test$dataset <- "test"
+x_train$dataset <- "train"
+y_test$dataset <- "test"
+y_train$dataset <-"train"
+subject_test <- "test"
+subject_train <- "train"
+
+
+ds_X <- rbind(x_test,x_train)
 ds_Y <- rbind(y_test,y_train)
 ds_subj <- rbind(subject_test,subject_train)
-rm(X_test, X_train, y_test, y_train, subject_test, subject_train)
-tbl_df(ds_Y)
+rm(x_test, x_train, y_test, y_train, subject_test, subject_train)
 
 #2. Extracts columns containing mean and standard deviation for each measurement
 #(Hint: Since some feature/column names are repeated, you may need to use the
@@ -45,8 +52,6 @@ ds_Y$ActivityName <-  ifelse(ds_Y$ActivityLabel == "1", "WALKING",
                               ifelse(ds_Y$ActivityLabel == "5", "STANDING", 
                                 ifelse(ds_Y$ActivityLabel == "6", "LAYING",NA))))))
 
-View(ds_Y)
-View(features)
 
 #4. From the data set in step 3, creates a second, independent tidy data set
 #with the average of each variable for each activity and each subject.
